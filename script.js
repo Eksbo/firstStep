56663d2f3337ce160aeea84146667f6ebdc477f9
Eksbo <bogdan.eksbo@gmail.com>
2022-08-01 20:03:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,118 @@
+"use strict";
+// let a ='5.5px' ;
+// let b = "6.25px" ;
+// console.log(parseFloat(a) + parseFloat(b) + "px");
+
+// let a = 234 ;
+// let b = 44 ;
+// console.log(String(a).length + String(b).length);
+
+
+// let test = 'abcdefghijklmnopqrstuvwxyz'
+// console.log(test[0]);
+// console.log(test[1]);
+// console.log(test[2]);
+
+// Дана строка '12345'. Найдите сумму цифр этой строки. 
+// let a ='12345' ;
+// let sum = Number(a[0]) + Number(a[1]) + Number(a[2]) + Number(a[3]) + Number(a[4]) ;
+// console.log(sum);
+
+// Дано число 12345. Найдите сумму цифр этого числа. 
+// let a =12345
+// let b = String(a)
+// let c = Number(b[0]) + Number(b[1]) + Number(b[2]) + Number(b[3]) + Number(b[4])
+// console.log(c);
+
+//Дано число 12345. Найдите произведение цифр этого числа. 
+// let a =12345 ;
+// let b = String(a) ;
+// let c = b[0] * b[1] * b[2] * b[3] * b[4] ;
+// console.log(c);
+
+// Дано число 12345. Переставьте цифры этого числа в обратном порядке. 
+// let a = 12345 ;
+// let b = String(a)
+// let c = Number(b[4] +  b[3] + b[2] + b[1] +b[0] );
+// console.log(c);
+// console.log(typeof(c));
+
+// Переделайте этот код так, чтобы в нем использовались описанные сокращенные операции: 
+// let num = 47;
+// num = num + 7;
+// num = num - 18;
+// num = num * 10;
+// num = num / 15;
+// alert(num);
+// let num = 47;
+// num += 7;
+// num -= 18;
+// num *= 10;
+// num /= 15;
+// alert(num);
+
+// Переделайте этот код так, чтобы в нем операции инкремента и декремента: 
+// let num = 10;
+// num = num + 1;
+// num = num + 1;
+// num = num - 1;
+// alert(num);
+
+// let num = 10;
+// num ++ ;
+// num ++ ;
+// num --;
+// alert(num);
+
+// специальный метод toFixed, выполняющий округление
+// let a = 0.1 + 0.2 ;
+// alert(a.toFixed(2));
+
+// Переделайте приведенный выше код так, чтобы суммировались числа, а не строки. 
+// let num1 = prompt('Введите первое число');
+// let num2 = prompt('Введите второе число');
+
+// alert(num1 + num2); // сложит числа как строки
+
+
+// let num1 = prompt('Введите первое число');
+// let num2 = prompt('Введите второе число');
+
+// alert(Number(num1) + Number(num2));
+
+// С помощью функции prompt спросите у пользователя сторону квадрата.
+//  Выведите на экран площадь введенного квадрата. 
+
+// let num1 = prompt('Введите сторону квадрата');
+// alert(Number(num1) * Number(num1) + "cm2")
+
+
+
+// С помощью двух функций prompt спросите у 
+// пользователя стороны прямоугольника.
+//  Выведите на экран периметр введенного прямоугольника. 
+
+// let num1 = prompt('Введите первую сторону');
+// let num2 = prompt('Введите вторую сторону');
+// let c = Number(num1) * Number(num2) ;
+// alert( c + "cm2")
+
+
+//  Выведите в окно браузера какой-нибудь текст. 
+// document.write('HELLO WORLD');
+// Выведите в окно браузера какой-нибудь текст, сделав его курсивным. 
+
+// document.write(`<hr> <b> HELLO WORLD </b>`);
+
+// Выведите текст из переменной в окно браузера так, чтобы он был курсивным. 
+
+// let str = 'text';
+// document.write(`<b> ${str} </b>`)
+
+// Выведите в окно браузера столбец чисел от 1 до 5. 
+
+// document.write(
+//     `<ul> <li> 1 </li> <li> 2 </li> <li> 3 </li> <li> 4 </li> <li> 5 </li> </ul>`
+
+// )
+
